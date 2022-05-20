a21de532f55fc21cee6f2a112fa244092a87b02d
[Dylan Kotzer] <[dKotzer@gmail.com]>
2022-05-20 17:59:58 -0400

    first real commit

---
+++ bot.js
@@ -0,0 +1,54 @@
+require("dotenv").config();
+const Discord = require("discord.js");
+const client = new Discord.Client({
+  intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES],
+  partials: ["MESSAGE"],
+});
+
+const BOT_PREFIX = "!";
+const MOD_ME_COMMAND = "mod-me";
+
+client.on("ready", () => {
+  console.log("Our bot is ready to go!!!!");
+});
+
+client.on("messageDelete", (msg) => {
+  msg.channel.send("Stop deleting messages");
+});
+
+client.on("message", (msg) => {
+  if (msg.content == "Jon") {
+    msg.react("❤️");
+  }
+
+  if (msg.content === `${BOT_PREFIX}${MOD_ME_COMMAND}`) {
+    modUser(msg.member);
+  }
+});
+client.on("message", (msg) => {
+  if (msg.content == "Camiel") {
+    msg.react("❤️");
+  }
+
+  if (msg.content === `${BOT_PREFIX}${MOD_ME_COMMAND}`) {
+    modUser(msg.member);
+  }
+});
+
+client.on("message", (msg) => {
+  if (msg.content == "Dylan") {
+    msg.react("❤️");
+  }
+
+  if (msg.content === `${BOT_PREFIX}${MOD_ME_COMMAND}`) {
+    modUser(msg.member);
+  }
+});
+
+client.on('')
+
+function modUser(member) {
+  member.roles.add("783084095223234590");
+}
+
+client.login(process.env.BOT_TOKEN);
