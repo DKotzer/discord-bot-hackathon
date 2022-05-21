62167fc2d951696da8aa16db465972817b9976fe
Jonathan Toye <jontoye@gmail.com>
2022-05-20 22:31:57 -0400

    add deploy-commands file

--- bot.js
+++ bot.js
@@ -36,6 +36,22 @@ client.on("message", (msg) => {
   }
 });
 
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  const { commandName } = interaction;
+
+  if (commandName === 'ping') {
+    await interaction.reply('Pong!');
+  } else if (commandName === 'server') {
+    await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+  } else if (commandName === 'user') {
+    await interaction.reply('User info.');
+  } else if (commandName === 'test') {
+    await interaction.reply('this was a test')
+  }
+})
+
 // client.on("guildMemberAdd", (message, member) => {
 //   console.log("new user found", member);
 //   message.channel.send("Welcome!", member.name);
