72ae764705ef01f00d9ff8a71d67aafcf9fee1db
[Dylan Kotzer] <[dKotzer@gmail.com]>
2022-05-20 21:14:55 -0400

    small updates

--- bot.js
+++ bot.js
@@ -17,38 +17,37 @@ client.on("messageDelete", (msg) => {
 });
 
 client.on("message", (msg) => {
+  console.log(msg.author.username);
   if (msg.content == "Jon") {
     msg.react("❤️");
   }
-
-  if (msg.content === `${BOT_PREFIX}${MOD_ME_COMMAND}`) {
-    modUser(msg.member);
-  }
-});
-client.on("message", (msg) => {
   if (msg.content == "Camiel") {
     msg.react("❤️");
   }
-
-  if (msg.content === `${BOT_PREFIX}${MOD_ME_COMMAND}`) {
-    modUser(msg.member);
-  }
-});
-
-client.on("message", (msg) => {
   if (msg.content == "Dylan") {
     msg.react("❤️");
   }
+  if (msg.content.includes("test")) {
+    msg.channel.send("stop doing that");
+  }
 
   if (msg.content === `${BOT_PREFIX}${MOD_ME_COMMAND}`) {
     modUser(msg.member);
   }
 });
+
+// client.on("guildMemberAdd", (message, member) => {
+//   console.log("new user found", member);
+//   message.channel.send("Welcome!", member.name);
+// });
 
 // client.on("");
 
 function modUser(member) {
-  member.roles.add("783084095223234590");
+  member.roles.add("977323882581205053");
 }
-
+test = {
+  name: "bob",
+};
 client.login(process.env.BOT_TOKEN);
+client.emit("guildMemberAdd");
